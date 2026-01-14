691041bf5202d8d0b39d215760099e04bda7b2a0
Giorgi kintsurashvili <kintsurashviligaga@gmail.com>
2026-01-14 17:25:37 +0400

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,1 +1,31 @@
+import { type ClassValue } from 'clsx'
 
+export function cn(...inputs: ClassValue[]) {
+  return inputs.filter(Boolean).join(' ')
+}
+
+export function formatDate(date: Date): string {
+  const now = new Date()
+  const diffInSeconds = Math.floor((now.getTime() - date.getTime()) / 1000)
+
+  if (diffInSeconds < 60) {
+    return 'Just now'
+  }
+
+  const diffInMinutes = Math.floor(diffInSeconds / 60)
+  if (diffInMinutes < 60) {
+    return `${diffInMinutes}m ago`
+  }
+
+  const diffInHours = Math.floor(diffInMinutes / 60)
+  if (diffInHours < 24) {
+    return `${diffInHours}h ago`
+  }
+
+  const diffInDays = Math.floor(diffInHours / 24)
+  if (diffInDays < 7) {
+    return `${diffInDays}d ago`
+  }
+
+  return date.toLocaleDateString()
+}
